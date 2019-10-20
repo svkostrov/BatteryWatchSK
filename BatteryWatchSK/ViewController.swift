ead0dd059dbe09a89657d205c3b07a9e38f1fc09
Sergey Kostrov <rokotx@bk.ru>
2019-10-21 00:14:46 +0300

    Initial Commit

---
+++ BatteryWatchSK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BatteryWatchSK
+//
+//  Created by Сергей Костров on 21.10.2019.
+//  Copyright © 2019 Сергей Костров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
